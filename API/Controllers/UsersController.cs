02a4e5f77c0fd807de002d58560d1290377c46df
Thanasis Andritsios <thanasis1997abc@gmail.com>
2024-06-10 21:59:55 +0300

    Initial Commit

---
+++ API/Controllers/UsersController.cs
@@ -0,0 +1,30 @@
+﻿using API.Data;
+using API.Entities;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace API.Controllers;
+
+public static class UsersController 
+{
+    public  static void ConfigApi(this WebApplication app){
+        app.MapGet("/Users",GetUsers);
+        app.MapGet("/Users/{id}",GetUser);
+    }
+        private static async Task<IResult> GetUser([FromServices] DataContext context, int id)
+        {
+            var user = await context.Users.FindAsync(id);
+            if (user == null)
+            {
+                return Results.NotFound();
+            }
+            return Results.Ok(user);
+        }
+
+        private static async Task<IEnumerable<AppUser>> GetUsers([FromServices] DataContext context)
+        {
+            var users = await context.Users.ToListAsync();
+            return users;
+        }
+
+}
